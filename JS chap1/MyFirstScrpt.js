fea9765ea3b92c373b602b4975d4f9c06fe569a1
Franck Bulu <macfr@Francks-MacBook-Pro.local>
2021-04-19 17:58:09 +0200

    test commit

---
+++ JS chap1/MyFirstScrpt.js
@@ -0,0 +1,4 @@
+console.log('Hello "world"')
+let a = 15;
+let b = 16;
+console.log(a + b);
---
+++ JS chap1/sendBox.js
@@ -0,0 +1,82 @@
+// alert('Hello world');
+let age = 30;
+let year = 2020;
+
+
+console.log(age, year);
+age = 13000;
+console.log(age);
+
+const points = 199;
+//points = 59;
+console.log(points);
+
+var po = 500;
+po = 900;
+console.log(po);
+
+// strings
+console.log('strings stylz')
+let email = 'jam.stylz@gmail.com';
+console.log(email);
+
+// string concatenation
+let firstName = 'Jam';
+let lastName = 'Stylz';
+let fullName = firstName + ' ' + lastName;
+console.log(firstName + lastName);
+console.log(fullName);
+
+// getting characteres
+//console.log(fullName[7]);
+
+// string length
+//console.log(fullName.length);
+
+// string methods
+//console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+
+//console.log(result, fullName);
+// let index = email.indexOf('@');
+// let result = fullName.slice(1, 4);
+// let result = fullName.substr(5, 8);
+let result = fullName.replace('S ', 'r');
+
+console.log(result);
+
+// numbers
+
+let radius = 10;
+const pi = 3.14;
+
+console.log(radius, pi);
+// math operator
+console.log(10 / 2);
+let calc = radius % 3;
+console.log(calc);
+
+
+// template strings
+const title = 'best reads of 2020';
+const author = 'Franck';
+const likes = 30;
+
+// concatenation way
+let res = 'The blog called' + title + 'by' + author + 'get' + likes + 'likes';
+console.log(res);
+
+// template string way
+
+
+// creating html way
+
+let a = 2;
+let b = 3;
+console.log(a + b);
+
+
+for (let i = 11; i > 10; i++){
+    //console.log(i);
+}
+
